1814257aed54103b8ea4ed1877b298503a0697ef
newasfuck <tarobo@naver.com>
2025-08-12 00:50:12 +0900

    Update PluginProcessor.cpp

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -1,1 +1,8 @@
 // Minimal processor placeholder
+#include "PluginProcessor.h"
+
+// JUCE looks for this at link time for every wrapper (AU/VST3).
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new HappyAccidentAudioProcessor();
+}
