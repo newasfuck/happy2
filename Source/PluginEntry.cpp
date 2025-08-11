ec184c254b58966fd854234e54cfba41200e04b4
newasfuck <tarobo@naver.com>
2025-08-12 00:43:48 +0900

    Update PluginEntry.cpp

--- Source/PluginEntry.cpp
+++ Source/PluginEntry.cpp
@@ -1,8 +1,8 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
-// JUCE needs this to instantiate your processor for each wrapper (AU/VST3).
-std::unique_ptr<juce::AudioProcessor> JUCE_CALLTYPE createPluginFilter()
+// JUCE expects a RAW POINTER here, not std::unique_ptr.
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
 {
-    return std::make_unique<HappyAccidentAudioProcessor>();
+    return new HappyAccidentAudioProcessor();
 }
