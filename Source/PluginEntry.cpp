8c32718186b3301846efd2e3b4d5f6d3de4da44d
newasfuck <tarobo@naver.com>
2025-08-12 00:51:10 +0900

    Delete Source/PluginEntry.cpp

--- Source/PluginEntry.cpp
+++
@@ -1,8 +0,0 @@
-#include <JuceHeader.h>
-#include "PluginProcessor.h"
-
-// JUCE expects a RAW POINTER here, not std::unique_ptr.
-juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
-{
-    return new HappyAccidentAudioProcessor();
-}
